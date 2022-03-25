8ac9c4002d05978d0b82fb0f7e24edec83525e2d
Emessias <developermessias@gmail.com>
2022-03-25 17:06:14 -0300

    tela-listar-jogadores

--- src/views/ListPlayers.vue
+++ src/views/ListPlayers.vue
@@ -1,14 +1,53 @@
 
 <template>
-  <h1>List of all players</h1>
+  <h2>Lista de jogadores</h2>
+  <el-row justify="auto">
+    <el-card 
+      class="box-card"
+      v-for="jogador, id of lista" 
+      :key="id">
+      <el-row>
+        <el-col :span="24">
+          <h2>{{jogador.nome}}</h2>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24">
+          <el-image :src="jogador.foto" :fit="contain">
+          </el-image>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24">
+          <h2>{{'Pontuação: 6'}}</h2>
+        </el-col>
+      </el-row>
+    </el-card>
+  </el-row>
 </template>
 
 <script>
 export default {
+  name: 'list-players',
+  data(){
+    return{
+      lista: []
+    }
+  },
+ 
+  mounted(){
+    let listaJogadores = JSON.parse(localStorage.localDb)
+    this.lista = listaJogadores.voleifranca.jogadores
+  }
 
 }
+
 </script>
 
-<style>
+<style scoped>
+
+.box-card {
+  width: 450px;
+}
 
 </style>
